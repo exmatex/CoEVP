c298d47ef0eadd1dcbf1b17be0504ef3b9b56b70
Sue Mniszewski <smm@lanl.gov>
2015-06-11 12:27:42 -0600

    Moved MPI init/shutdeon to main.

--- LULESH/lulesh_main.cc
+++ LULESH/lulesh_main.cc
@@ -1,10 +1,30 @@
 #include "lulesh.h"
 
+#if defined(COEVP_MPI)
+#include <mpi.h>
+#endif
+
 int main(int argc, char *argv[])
 {
+#if defined(COEVP_MPI)
+   int numRanks ;
+   int myRank ;
+   MPI_Init(&argc, &argv) ;
+   MPI_Comm_size(MPI_COMM_WORLD, &numRanks) ;
+   MPI_Comm_rank(MPI_COMM_WORLD, &myRank) ;
+#endif
+
   Lulesh luleshSystem;
 
-  luleshSystem.go(argc, argv);
+#if defined(COEVP_MPI)
+  luleshSystem.go(myRank, numRanks); 
+#else
+  luleshSystem.go();
+#endif
+
+#if defined(COEVP_MPI)
+   MPI_Finalize() ;
+#endif
 
   return 0;
 }
