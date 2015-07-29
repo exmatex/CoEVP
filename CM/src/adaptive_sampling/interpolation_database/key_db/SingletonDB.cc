3694df30758437a4be996895b9dbda7786749052
Allen McPherson <mcpherson@lanl.gov>
2015-07-29 12:53:20 -0600

    Moved singleton code into CC file.

--- CM/src/adaptive_sampling/interpolation_database/key_db/SingletonDB.cc
+++ CM/src/adaptive_sampling/interpolation_database/key_db/SingletonDB.cc
@@ -1,1 +1,70 @@
-// Empty
+//
+//  First attempt at using a global (singleton) redis connections.
+//  This WILL evolve--it's mostly hack code now.  For instance,
+//  we'll likeliy refactor to a base class so that we can suport
+//  many types of databases.
+//
+// http://stackoverflow.com/questions/1008019/c-singleton-design-pattern
+
+#include "SingletonDB.h"
+
+#define REDIS_PORT 6379
+#define REDIS_HOST "localhost"
+
+#include <iostream>
+#include <string>
+#include <assert.h>
+
+
+//  Will eventually be something like add_points
+void  SingletonDB::sadd_sb(const char *key, const std::vector<double>& buf) {
+  redisReply* reply;
+  unsigned long sz = buf.size();
+  reply = (redisReply *)redisCommand(redis, "SADD %s %b%b",
+                                     key, &sz, sizeof(unsigned long),
+                                     &buf[0], buf.size()*sizeof(double));
+  if (!reply) {
+    throw std::runtime_error("No connection to redis server, please start one on host'"
+                             + std::string(REDIS_HOST) + "' and port "
+                             + std::to_string(REDIS_PORT));
+  }
+  freeReplyObject(reply);
+}
+
+
+//  Will eventually be something like get_points
+std::vector<double> SingletonDB::smembers_s(const char *key) {
+  redisReply* reply;
+  reply = (redisReply *)redisCommand(redis, "SMEMBERS %s", key);
+  if (!reply) {
+    throw std::runtime_error("No connection to redis server, please start one on host'"
+                             + std::string(REDIS_HOST) + "' and port "
+                             + std::to_string(REDIS_PORT));
+  }
+  if (reply->type != REDIS_REPLY_ARRAY){
+    throw std::runtime_error("Wrong redis return type");
+  }
+  //TODO not sure what to do with other replies
+  if(reply->elements < 1) {
+    throw std::runtime_error("Number of redis reply elements wrong, got: "
+                             + std::to_string(reply->elements));
+  }
+  unsigned long *sz = (unsigned long *)(reply->element)[0]->str;
+  double *raw=(double *)(sz+1);
+  std::vector<double> packedContainer;
+  packedContainer.assign(raw, raw + *sz);
+  freeReplyObject(reply);
+  return packedContainer;
+}
+
+
+SingletonDB::SingletonDB() {
+  redis = redisConnect(REDIS_HOST, REDIS_PORT);
+  std::cout << "Connecting to redis in SingletonDB..." << std::endl;
+}
+
+
+SingletonDB::~SingletonDB() {
+  redisFree(redis);
+  std::cout << "Closing redis in SingletonDB..." << std::endl;
+}
--- CM/src/adaptive_sampling/interpolation_database/key_db/SingletonDB.h
+++ CM/src/adaptive_sampling/interpolation_database/key_db/SingletonDB.h
@@ -4,69 +4,29 @@
 //  we'll likeliy refactor to a base class so that we can suport
 //  many types of databases.
 //
-// Allen McPherson
-// Los Alamos National Laboratory
-
 // http://stackoverflow.com/questions/1008019/c-singleton-design-pattern
 
-#define REDIS_PORT 6379
-#define REDIS_HOST "localhost"
-
-#include <iostream>
-#include <string>
 #include <vector>
-#include <assert.h>
 #include <hiredis.h>
 
 class SingletonDB {
  public:
+  
+  //  Return the single instance that was initialized in the private constructor.
   static  SingletonDB&  getInstance() {
     static  SingletonDB   instance;
     return instance;
   }
-  void  sadd_sb(const char *key, const std::vector<double>& buf) {
-    redisReply* reply;
-    unsigned long sz = buf.size();
-    reply = (redisReply *)redisCommand(redis, "SADD %s %b%b", key, &sz, sizeof(unsigned long), &buf[0], buf.size()*sizeof(double));
-    if (!reply) {
-      throw std::runtime_error("No connection to redis server, please start one on host'" + std::string(REDIS_HOST) + "' and port " + std::to_string(REDIS_PORT));
-    }
-    freeReplyObject(reply);
-  }
 
-  std::vector<double> smembers_s(const char *key) {
-    redisReply* reply;
-    //std::cout << "Connecting to redis in SingletonDB..." << std::endl;
-    reply = (redisReply *)redisCommand(redis, "SMEMBERS %s", key);
-    if (!reply) {
-      throw std::runtime_error("No connection to redis server, please start one on host'" + std::string(REDIS_HOST) + "' and port " + std::to_string(REDIS_PORT));
-    }
-    if (reply->type != REDIS_REPLY_ARRAY){
-      throw std::runtime_error("Wrong redis return type");
-    }
-    //TODO not sure what to do with other replies
-    if(reply->elements < 1) {
-      throw std::runtime_error("Number of redis reply elements wrong, got: " + std::to_string(reply->elements));
-    }
-    unsigned long *sz = (unsigned long *)(reply->element)[0]->str;
-    double *raw=(double *)(sz+1);
-    std::vector<double> packedContainer;
-    packedContainer.assign(raw, raw + *sz);
-    freeReplyObject(reply);
-    return packedContainer;
-  }
+  void  sadd_sb(const char *key, const std::vector<double>& buf);
+  std::vector<double> smembers_s(const char *key);
 
 private:
   redisContext*   redis;
 
-  SingletonDB() {
-    redis = redisConnect(REDIS_HOST, REDIS_PORT);
-    std::cout << "Connecting to redis in SingletonDB..." << std::endl;
-  }
-  ~SingletonDB() {
-    redisFree(redis);
-    std::cout << "Closing redis in SingletonDB..." << std::endl;
-  }
+  SingletonDB();
+  ~SingletonDB();
+
   //  This technique requires C++11 (can do a C++03 version too)
   SingletonDB(SingletonDB const&)    = delete;
   void operator=(SingletonDB const&) = delete;
