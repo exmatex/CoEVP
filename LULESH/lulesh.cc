723f5707c17f5aa06488f6dfa40976a7042245c3
Jeffrey A. Keasler <keasler@cab669.llnl.gov>
2015-12-08 12:54:32 -0800

    Fix lzetap connectivity bug.

--- LULESH/lulesh.cc
+++ LULESH/lulesh.cc
@@ -3329,6 +3329,7 @@ void Lulesh::go(int argc, char *argv[])
    }
 
    /* Create connectivity for lzetam, lzetap */
+#ifdef OLD_LZETA_CONNECTIVITY
    for (Index_t i=0; i<edgeElems*heightElems; ++i) {
       domain.lzetam(i) = i ;
       domain.lzetap(domElems-edgeElems*heightElems+i) =
@@ -3369,6 +3370,48 @@ void Lulesh::go(int argc, char *argv[])
                     coreElems*edgeElems*heightElems - wingElems*heightElems -
                     heightElems + col ;
    }
+#else
+   for (Index_t i=0; i<edgeElems*heightElems; ++i) {
+      domain.lzetam(i) = i ;
+      domain.lzetap(domElems-edgeElems*heightElems+i) =
+                            domElems-edgeElems*heightElems+i ;
+   }
+   for (Index_t i=edgeElems*heightElems;
+        i<coreElems*edgeElems*heightElems; ++i) {
+      domain.lzetam(i) = i - edgeElems*heightElems ;
+      domain.lzetap(i-edgeElems*heightElems) = i ;
+   }
+   /* patch lzetap to connect Y wing to Z wing */
+   for (int i=domElems - wingElems*heightElems; i<domElems; ++i) {
+      domain.lzetap(coreElems*edgeElems*heightElems - (domElems - i)) = i ;
+   }
+   /* set lzetam and lzetap for Z wing elements, minus notch plane */
+   for (int i=coreElems*edgeElems*heightElems;
+        i<coreElems*edgeElems*heightElems + (coreElems-1)*wingElems*heightElems;
+        ++i) {
+      domain.lzetam(i) = i - (coreElems-1)*heightElems ;
+      domain.lzetap(i) = i + (coreElems-1)*heightElems ;
+   }
+   /* patch lzetam */
+   for (int i=coreElems*edgeElems*heightElems;
+        i<coreElems*edgeElems*heightElems + (coreElems-1)*heightElems; ++i) {
+      domain.lzetam(i) = i - edgeElems*heightElems ;
+   }
+   /* set lzetam, lzetap for notch plane */
+   for (int i=domElems-wingElems*heightElems; i<domElems; ++i) {
+      domain.lzetam(i) = i - heightElems ;
+      domain.lzetap(i) = i + heightElems ;
+   }
+   /* patch lzetap for notch plane row of elements */
+   for (int col=0; col<heightElems; ++col) {
+      domain.lzetap(coreElems*edgeElems*heightElems -
+                    wingElems*heightElems - heightElems + col) =
+                       domElems - wingElems*heightElems + col ;
+      domain.lzetam(domElems - wingElems*heightElems + col) =
+                    coreElems*edgeElems*heightElems - wingElems*heightElems -
+                    heightElems + col ;
+   }
+#endif
 
    /* set up boundary condition information */
    for (Index_t i=0; i<domElems; ++i) {
