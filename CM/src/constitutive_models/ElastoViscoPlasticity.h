7f6b4960ff20d8a70256a8a1d01d49c72c89b838
Milo Dorr <dorr1@llnl.gov>
2015-12-22 10:28:53 -0800

    Adding three double Taylor "state" to the CM opaque blob.
    Adding an abstract getState(void* state) member to the Plasticity class.
    
    Removing const modifiers from the plasticity models and their advance() member.

--- CM/src/constitutive_models/ElastoViscoPlasticity.h
+++ CM/src/constitutive_models/ElastoViscoPlasticity.h
@@ -19,7 +19,7 @@ class ElastoViscoPlasticity
                              const double            bulk_modulus,
                              const double            shear_modulus,
                              const EOS*              eos,
-                             const Plasticity*       fine_scale_model,
+                             Plasticity*             fine_scale_model,
                              const bool              use_adaptive_sampling,
                              size_t&                 state_size );
 
@@ -134,6 +134,10 @@ class ElastoViscoPlasticity
                                   Tensor2Sym&       Dbar_prime,
                                   Tensor4LSym&      Dbar_prime_deriv ) const;
 
+      size_t getKinematicVariableStateSize() const;
+
+      void getKinematicVariableState( void* buffer ) const;
+
       double dot( const Tensor2Sym& tensor1,
                   const Tensor2Sym& tensor2 ) const;
       double norm2( const Tensor2Sym& tensor ) const {return dot(tensor,tensor);}
@@ -165,7 +169,7 @@ class ElastoViscoPlasticity
 
       const EOS* m_eos_model;   // Equation of state model
 
-      const Plasticity* m_plasticity_model;  // Fine-scale model
+      Plasticity* m_plasticity_model;  // Fine-scale model
 };
 
 #endif
