3fc979523f76305efcc4c21f68f2687b58ac6fa5
Robert Pavel <rspavel@lanl.gov>
2016-01-28 14:18:28 -0700

    Cleaned up LULESH Interface to Support Backends

--- CM/src/adaptive_sampling/interpolation_database/kriging_database/SingletonDB_Dummy.cc
+++ CM/src/adaptive_sampling/interpolation_database/kriging_database/SingletonDB_Dummy.cc
@@ -4,36 +4,34 @@
 
 SingletonDB_Dummy::SingletonDB_Dummy(bool dummyArg)
 {
-	std::cerr << "Error: Instantiating Dummy DB Backend. Check your build flags" << std::endl;
+    throw std::runtime_error("Error: Instantiation Dummy DB Backend. Check your build flags"); 
 }
 
 SingletonDB_Dummy::~SingletonDB_Dummy()
 {
-	std::cerr << "Error: Deleting Dummy DB Backend. Check your build flags" << std::endl;
+    throw std::runtime_error("Error: Instantiation Dummy DB Backend. Check your build flags"); 
 }
 
 void  SingletonDB_Dummy::push(const uint128_t &key, const std::vector<double>& buf, const unsigned long key_length)
 {
-	std::cerr << "Error: Pushing to Dummy DB Backend. Check your build flags" << std::endl;
-
+    throw std::runtime_error("Error: Instantiation Dummy DB Backend. Check your build flags"); 
 }
 
 void  SingletonDB_Dummy::erase(const uint128_t &key)
 {
-	std::cerr << "Error: Erasing from Dummy DB Backend. Check your build flags" << std::endl;
-
+    throw std::runtime_error("Error: Instantiation Dummy DB Backend. Check your build flags"); 
 }
 
 std::vector<double> SingletonDB_Dummy::pull(const uint128_t &key)
 {
-	std::cerr << "Error: Reading from Dummy DB Backend. Check your build flags" << std::endl;
+    throw std::runtime_error("Error: Instantiation Dummy DB Backend. Check your build flags"); 
 	std::vector<double> retVec;
 	return retVec;
 }
 
 std::vector<double> SingletonDB_Dummy::pull_key(const uint128_t &key)
 {
-	std::cerr << "Error: Reading from Dummy DB Backend. Check your build flags" << std::endl;
+    throw std::runtime_error("Error: Instantiation Dummy DB Backend. Check your build flags"); 
 	std::vector<double> retVec;
 	return retVec;
 }
