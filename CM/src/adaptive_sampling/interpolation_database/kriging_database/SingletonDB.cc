44828180c6273104f2819308699a1b4dee57eace
Robert Pavel <rspavel@lanl.gov>
2016-02-08 09:41:23 -0700

    Committing to Push Branch as Reference Info

--- CM/src/adaptive_sampling/interpolation_database/kriging_database/SingletonDB.cc
+++ CM/src/adaptive_sampling/interpolation_database/kriging_database/SingletonDB.cc
@@ -18,18 +18,38 @@ typedef SingletonDB_Dummy SingletonDB_Redis;
 #endif // REDIS
 
 #include <iostream>
+#include <fstream>
+extern std::ofstream logFile;
+
+
+std::string to_string_hack2(unsigned long long inVal)
+{
+	char buf[256];
+	sprintf(buf, "%llu", inVal);
+	std::string retString(buf);
+	return buf;
+}
+
+static std::string uint128_to_string2(const uint128_t &in){
+   uint64_t *in64 = (uint64_t *)&in; 
+   return to_string_hack2((unsigned long long) *in64)+to_string_hack2((unsigned long long)*(in64+1));
+}
+
 
     
 //  Will eventually be something like add_points
 void  SingletonDB::push(const uint128_t &key, const std::vector<double>& buf, const unsigned long key_length) {
+	logFile << "WRITE\t" << uint128_to_string2(key) << std::endl;
   this->backend->push(key, buf, key_length);
 }
 
 void  SingletonDB::erase(const uint128_t &key){
+	logFile << "ERASE\t" << uint128_to_string2(key) << std::endl;
   this->backend->erase(key);
 }
 
 std::vector<double> SingletonDB::pull(const uint128_t &key) {
+	logFile << "READ\t" << uint128_to_string2(key) << std::endl;
   return this->backend->pull(key);
 }
 
