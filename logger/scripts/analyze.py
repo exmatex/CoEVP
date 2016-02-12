7a1b4ec42def5fe6e578cc4d9d88158510ceba8b
Allen L. McPherson <mcpherson@lanl.gov>
2016-02-12 09:57:11 -0700

    Starting analyze code

--- logger/scripts/analyze.py
+++ logger/scripts/analyze.py
@@ -4,8 +4,19 @@ import pandas as pd
 
 def main():
     df = pd.read_csv('dump.csv')
-    print(df)
-    
+    print(df.info())
+    print("\n\n")
+    print(df.dtypes)
+    print("\n\n")
+    print(df.describe())
+    print("\n\n")
+    timers =   df[(df.Mode == 'TIMER')]
+    counters = df[(df.Mode == 'COUNT')]
+    print(timers)
+    print("\n\n")
+    print(counters)
+    print("\n\n")
 
+    
 if __name__ == "__main__":
     main()
